c38747b7a3324748c7f8810d6944e328480e9166
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-03 19:44:21 +0100

    localstorage

--- src/routes/projects/clicker/+page.svelte
+++ src/routes/projects/clicker/+page.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
-	let count = 0;
+	import { writable } from 'svelte/store';
+	let money = 0;
+	let cursor_amount = 0;
 	function addOne() {
-		count += 1;
+		money += 1;
+		localStorage.setItem('money', `${money}`);
+		localStorage.setItem('cursor_amount', `${cursor_amount}`);
 	}
+
+	function buycursor() {
+		if (money < 200) return;
+		money -= 200;
+		cursor_amount += 1;
+	}
+	function cursor() {
+		localStorage.setItem('money', `${money}`);
+		localStorage.setItem('cursor_amount', `${cursor_amount}`);
+		money += 1 * cursor_amount;
+	}
+
+	setInterval(cursor, 500);
 </script>
 
 <div class="flex flex-col items-center justify-center h-[95vh]">
-	<button on:click={addOne}>Clicked {count} Times</button>
+	<!-- gui -->
+	<div class="text-center">
+		<h1>{money}</h1>
+		<button on:click={addOne} class="w-32 h-32 bg-slate-600 rounded-2xl" />
+		<div>
+			<button on:click={buycursor}>buy cursor</button>
+		</div>
+	</div>
 </div>
