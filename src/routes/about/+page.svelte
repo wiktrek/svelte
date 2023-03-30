d1be4753101c6e502e212ac2f016b742f4febe83
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-30 13:40:22 +0200

    little update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,12 @@
 <head>
 	<title>about - svelte.wiktrek.xyz</title>
 </head>
-<a href="https://github.com/wiktrek/svelte">repo</a>
+<body>
+	<div class="justify-center text-center items-center object-center flex">
+		<div class="grid">
+			<a href="https://github.com/wiktrek/svelte">repo</a>
+			<p>Svelte app made with vite</p>
+			<img src="/vite.png" alt="vite" height="60" width="60" />
+		</div>
+	</div>
+</body>
