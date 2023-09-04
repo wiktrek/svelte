2d781e27928541ab5cf7344d63964016dcf804dd
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-04 20:06:26 +0200

    .

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+  import "../app.css";
+</script>
+<div class="bg-[#161718] h-screen text-[#fbfdfe]">
+  <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen">
+    <p></p>
+    <a href="https://wiktrek.xyz" target="_blank">wiktrek.xyz</a>
+</div>
