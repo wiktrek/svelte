b8a40997c427e6e828e234d4cf319f1499c7122e
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-06 20:15:56 +0200

    todo"

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+const todos: string[] = [];
+let error = ""
+let todo = ""
+function submit() {
+if (todos.includes(todo)) {
+console.log("Err")
+return error == "Error to do already exists"
+}
+todos.push(todo)
+
+console.log(todo, todos)
+}
+
+function save() {
+
+}
+</script>
+<div>
+<input placeholder="enter to do name" class="bg-transparent" id="name" bind:value={todo}/>
+<button class="bg-[#0cabc0] rounded" on:click={submit}>Add to do</button>
+<p>{error}</p>
+</div>
