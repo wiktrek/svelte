5178cdebaffdf970bcd01baba8b572a70defc303
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-11 23:56:58 +0200

    time works

--- src/routes/expense/+page.svelte
+++ src/routes/expense/+page.svelte
@@ -3,10 +3,13 @@
 
 
 <script lang="ts">
+// time = minutes
 interface Expense {
 amount: number;
-date: string;
+currency: string;
+time: number;
 }
+
 interface Day {
 date: string;
 expenses: Expense[];    
@@ -22,8 +25,11 @@ let example: Month = {
         date: "11",
         expenses: [{
             amount: 10,
-            date: "11"
-        }]
+            currency: "zł",
+            time: 720
+        },
+        { amount: 100, currency: "zł", time: 721}
+    ]
     }]
 }
 expenses.push(example)
@@ -36,6 +42,17 @@ function save() {
 let json = JSON.stringify(expenses);
 localStorage.setItem("expenses", json)
 }
+function min_to_h(m: number): string{
+let result: string = "";
+let h: number = Number((m / 60).toFixed())
+let minutes = m - (h * 60)
+if (minutes < 10) {
+result = `${h}:0${minutes}`
+return result
+}
+result = `${h}:${minutes}`
+return result
+}
 </script>
 <div>
 <p>Expenses</p>
@@ -44,7 +61,8 @@ localStorage.setItem("expenses", json)
     {#each expense.expenses as day}
     <p>day: {day.date}</p>
         {#each day.expenses as expense}
-        <p>amount spent: {expense.amount}</p>
+        
+        <p>{min_to_h(expense.time)} amount spent: {expense.amount}{expense.currency}</p>
         {/each}
     {/each}
 {/each}
