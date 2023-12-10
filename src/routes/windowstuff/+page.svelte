5453f1d4dd6291f60530c3f97041c9cd488a53c6
wiktrek <wiktor.janiszewski10@gmail.com>
2023-12-10 23:00:45 +0100

    feat: windows

--- src/routes/windowstuff/+page.svelte
+++ src/routes/windowstuff/+page.svelte
@@ -2,20 +2,44 @@
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { createScene } from './three';
-	let windows: number[] = [];
+	interface Window {
+		id: number;
+		prev: number;
+	}
+	let windows: Window[] = [];
 	const id = (Math.random() * 100).toFixed();
+	let prev = 0;
 	let xy: string = '[]';
 	if (typeof localStorage !== `undefined`) {
-		localStorage.setItem(`windows`, ``);
+		if (localStorage.getItem('windows') != null) {
+			windows = JSON.parse(localStorage.getItem('windows') as string) as Window[];
+		}
+
+		windows.push({ id: Number(id), prev: prev } as Window);
+
+		localStorage.setItem(`windows`, `${JSON.stringify(windows)}`);
+
 		console.log();
 		setInterval(() => {
 			xy = `[${window.screenX}, ${window.screenY}]`;
 			localStorage.setItem(`window ${id}`, xy);
+			windows = JSON.parse(localStorage.getItem('windows') as string) as Window[];
+			localStorage.setItem('windows', `${JSON.stringify(windows)}`);
 		}, 100);
 	}
 	if (typeof window !== `undefined`) {
 		window.addEventListener('beforeunload', function (e) {
 			// windows = Number(localStorage.getItem(`windows`));
+			const index = windows.indexOf({
+				id: Number(id),
+				prev: prev
+			} as Window);
+			if (index > -1) {
+				windows.splice(index, 1);
+			}
+
+			localStorage.removeItem(`windows`);
+			localStorage.setItem(`windows`, `${JSON.stringify(windows)}`);
 			localStorage.removeItem(`window ${id}`);
 		});
 		createScene();
